8c93a704a49f6ca7694e3e49030a360cdad5e678
Zohaib <zohaib.anwar0310@gmail.com>
2024-03-24 01:16:48 +0500

    worked with basic types

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log("It works..");
+var add = function (number1, number2) {
+    return number1 + number2;
+};
+var result = add(5, 5);
+console.log("result:", result);
--- app.ts
+++ app.ts
@@ -1,1 +1,7 @@
-console.log("It works..");
+const add = (number1: number, number2: number) => {
+  return number1 + number2;
+};
+
+const result = add(5, 5);
+
+console.log("result:", result);
